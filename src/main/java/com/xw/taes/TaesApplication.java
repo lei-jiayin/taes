a9b5dc22e92104500304450bafe9e90975bddbd5
xw <3597906135@qq.com>
2020-07-17 10:38:38 +0800

    Initial commit

---
+++ src/main/java/com/xw/taes/TaesApplication.java
@@ -0,0 +1,13 @@
+package com.xw.taes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xw/taes/TaesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xw.taes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
